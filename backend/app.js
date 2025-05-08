b26ea2bec609fc448ae5214d2c40b71b60a42ce0
Kodzukenken <abigail.01k4w@gmail.com>
2025-05-09 00:05:12 +0700

    missing port & routing

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,20 @@
 import express from 'express';
 import cors from 'cors';
-import router from './routes/routes.js';
+import bodyParser from 'body-parser';
+import linkRoutes from './routes/routes.js';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/links', router);
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use('/api/links', linkRoutes);
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Shorter API is runnin!'});
-});
+// app.get('/', (req, res) => {
+//     res.status(200).json({ message: 'Shorter API is runnin!'});
+// });
 
 app.listen(PORT, () => {
     console.log(`App is on port ${PORT}`)
